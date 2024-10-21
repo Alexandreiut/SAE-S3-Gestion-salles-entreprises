d01b89c2e944ffd1d1ea88bf7a82bd687233bb6a
NOE ARCIER <noe.arcier@iut.local>
2024-10-21 15:28:25 +0200

    Importateur sprint 1

--- workspace/RoomManager/src/modeles/reseau/Importateur.java
+++ workspace/RoomManager/src/modeles/reseau/Importateur.java
@@ -3,17 +3,28 @@ package modeles.reseau;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Arrays;
+
+import modeles.entree.LecteurCSV;
+import modeles.erreur.LectureException;
+import modeles.items.*;
+import modeles.stockage.Stockage;
+
 import java.io.PrintWriter;
 
 public class Importateur {
 	
-	/** socket associé à l'imortateur permettant une communication **/
+	/** socket associé à l'imortateur permettant une communication */
 	private Socket socketClient;
 	
-	/** entrée de l'importateur **/
+	/** entrée de l'importateur */
 	private BufferedReader input;
+	
+	/** stockage de l'application */
+	private Stockage stockage;
 	
 	/**
 	 * instancie un importateur, le socket est instancié selon l'adresse ip et le port 
@@ -54,7 +65,7 @@ public class Importateur {
 	 *  true si le transfert a été correctement transféré, false sinon
 	 * @return
 	 */
-	public ArrayList<String> recevoirDonnee() {
+	public ArrayList<String> recevoirDonnee() throws IOException {
 		
 		ArrayList<String> donnees = new ArrayList<>();
         
@@ -62,11 +73,7 @@ public class Importateur {
 		
 		paquet = "";
 		while (!paquet.equals("FIN")) {
-			try {
-				donnees.add(input.readLine());
-			} catch (IOException e) {
-				
-			}
+			donnees.addAll(Arrays.asList(input.readLine().split("\n")));
         }
 		
 		return donnees;
@@ -75,42 +82,71 @@ public class Importateur {
 	
 	/**
 	 * décrypte les données reçues et les convertis
-	 * en objets qui seront stockés,
+	 * en objets puis les stock,
 	 * true si le processus a correctement été effectué dans sa totalité,
 	 * false sinon
+	 * @throws FileNotFoundException si un fichier temporaire n'a pu être trouvé
 	 * @param donneAConvertir
 	 */
 	public boolean convertirReponseDonnee(ArrayList<String> donneAConvertir) {
 		
-		// TODO créer fichiers csv à partir de données
+		ArrayList<String> donneesFichier;
 		
+		ArrayList<Object> objetsAInserer;
 		
-		// TODO utiliser LecteurCSV et stockage pour créer objets
-		return false; //STUB
+		donneesFichier = null;
+		for (String ligne : donneAConvertir) {
+			
+			if (ligne.substring(0, 6).equals("Ident;")) {
+				
+				if (donneesFichier != null) {
+					// stockage des données du fichier précédent
+					try {
+						objetsAInserer = LecteurCSV.readFichier(donneesFichier);
+					} catch (Exception e) {
+						return false;
+					}
+					
+					if (objetsAInserer.get(0) instanceof Employe) {
+						stockage.setListeEmploye(objetsAInserer);
+					} else if (objetsAInserer.get(0) instanceof Activite) {
+						stockage.setListeActivite(objetsAInserer);
+					} else if (objetsAInserer.get(0) instanceof Salle) {
+						stockage.setListeSalle(objetsAInserer);
+					} else {
+						stockage.setListeReservation(objetsAInserer);
+					} 
+					
+				}
+				
+				donneesFichier = new ArrayList<String>();
+			} else {
+				donneesFichier.add(ligne);
+			}
+		}
+		
+		return true;
 	}
 	
 	
 	/**
-	 * 
-	 * @return
+	 * envoi l'entier au serveur
+	 * @return true si tout s'est bien passé, false sinon
 	 */
 	public boolean envoiEntier(int valeur) {
-		  try {
-	            PrintWriter output = new PrintWriter(socketClient.getOutputStream(), true);
-	            output.println(valeur); // Envoyer l'entier
-	            return true;
-	        } catch (IOException e) {
-	            return false;
-	        }
-	    
-		
-		
+	    try {
+            PrintWriter output = new PrintWriter(socketClient.getOutputStream(), true);
+            output.println(valeur); // Envoyer l'entier
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
 	}
 	
 	
 	/**
-	 * 
-	 * @return
+	 * reçois un entier du serveur
+	 * @return un entier
 	 */
 	public int recevoirEntier() {
 		return 0; //stub
@@ -124,9 +160,13 @@ public class Importateur {
 	 * @return
 	 */
 	public boolean closeConnexion() {
-		return true; //stub
 		
-		
+		try {
+			socketClient.close();
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
 	}
 	
 }
