894aa75f3b925c42c7d59a80bfd2a7ec8ea02fab
SamuelLacam <s.lacam46270@gmail.com>
2024-10-21 16:07:10 +0200

    bandeau en css de l'app + commencement page consultation

--- workspace/RoomManager/src/RoomManager.java
+++ workspace/RoomManager/src/RoomManager.java
@@ -1,23 +1,31 @@
 
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class RoomManager extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+            // Charge le fichier FXML
+            Parent root = FXMLLoader.load(getClass().getResource("affichages/RoomManager.fxml"));
+            
+            // Crée la scène avec la racine FXML
+            Scene scene = new Scene(root);
+            
+            // Ajoute la feuille de style CSS
+            //scene.getStylesheets().add(getClass().getResource("affichages/RoomManager.css").toExternalForm());
+            
+            // Configure et affiche la fenêtre
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
 	
 	public static void main(String[] args) {
