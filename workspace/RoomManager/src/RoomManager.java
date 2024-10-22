1cdd05a90d228482d232b8d3c66ee72646a0d986
SamuelLacam <s.lacam46270@gmail.com>
2024-10-23 01:17:01 +0200

    commencement controleur

--- workspace/RoomManager/src/RoomManager.java
+++ workspace/RoomManager/src/RoomManager.java
@@ -4,6 +4,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import modeles.NavigationVues;
 import modeles.sauvegarde.Serialisation;
 import modeles.stockage.Stockage;
 import javafx.scene.Parent;
@@ -29,11 +30,13 @@ public static Stockage stockage;
 	public void start(Stage primaryStage) {
 		try {
             // Charge le fichier FXML
-			//Parent root = FXMLLoader.load(getClass().getResource("affichages/RoomManager.fxml"));
-			Parent root = FXMLLoader.load(getClass().getResource("affichages/TestMenuLateral.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("affichages/RoomManager.fxml"));
+			//Parent root = FXMLLoader.load(getClass().getResource("affichages/TestMenuLateral.fxml"));
+			//Parent root = FXMLLoader.load(getClass().getResource("affichages/consultation.fxml"));
             
             // Crée la scène avec la racine FXML
             Scene scene = new Scene(root);
+            NavigationVues.setSceneCourante(scene);
             
             // Ajoute la feuille de style CSS
             //scene.getStylesheets().add(getClass().getResource("affichages/RoomManager.css").toExternalForm());
