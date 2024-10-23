885fa6907b6ab3a2ab4baa6cbcac26eaee596db5
Astier Adrien <adrien.astier@gmail.com>
2024-10-23 22:25:33 +0200

    Refator code + javadoc

--- workspace/RoomManager/src/controleurs/ExportateurControleur.java
+++ workspace/RoomManager/src/controleurs/ExportateurControleur.java
@@ -1,9 +1,13 @@
-/**
- *  
- * 
- * 
- *
- *
- *
- *
+/*
+ * ExportateurControleur.java				23/10/2024
+ * BUT Info2, 2024/2025, pas de copyright
  */
+
+package controleurs;
+
+/**
+ * Controleur de la vue exportateur.fxml
+ */
+public class ExportateurControleur {
+	
+}
