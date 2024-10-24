41b0c1c231ddf11974a2ad2c3acc2da00bd13f50
SamuelLacam <s.lacam46270@gmail.com>
2024-10-25 00:52:39 +0200

    refractor de consultation pour respecté les responsabilitées de chaque module  MVC

--- workspace/RoomManager/src/controleurs/ConsultationControleur.java
+++ workspace/RoomManager/src/controleurs/ConsultationControleur.java
@@ -6,12 +6,15 @@
 package controleurs;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import lanceur.RoomManager;
+
+import modeles.consultation.Consultation;
 import modeles.items.Activite;
 import modeles.items.Employe;
 import modeles.items.Reservation;
@@ -23,11 +26,6 @@ import modeles.stockage.Stockage;
  */
 public class ConsultationControleur {
 	
-	/**
-	 * Données de l'application à exploiter, les données sont stockées dans cette variable
-	 */
-	private static Stockage stockage;
-	
 	@FXML
 	private HBox hboxDonnees;
 	
@@ -36,70 +34,44 @@ public class ConsultationControleur {
 
 	
 	/**
-	 * Initialisation des données
+	 * Initialisation des données et affichage
+	 * par defaut lorsque l'on arrive sur la vue.
 	 */
 	@FXML
 	public void initialize() {
 		
-		stockage = RoomManager.stockage;
+		Consultation consultation = new Consultation();
+		HashMap<String,  ArrayList<? extends Object>> donnees = consultation.fetchDonneesBrutes();
 		
-		affichageDonnees();		
+		if (donnees.containsKey("pas de données")) {
+			affichage(vboxDonnees, donnees.get("pas de données"));
+		
+		} else {	
+			vboxDonnees.getChildren().add(new Label("Employés :"));
+			affichage(vboxDonnees, donnees.get("Employés"));
+			
+			vboxDonnees.getChildren().add(new Label("Salles :"));
+			affichage(vboxDonnees, donnees.get("Salles"));
+			
+			vboxDonnees.getChildren().add(new Label("Activitées :"));
+			affichage(vboxDonnees, donnees.get("Activitées"));
+			
+			vboxDonnees.getChildren().add(new Label("Réservations :"));
+			affichage(vboxDonnees, donnees.get("Réservations"));
+		}	
 	}
 	
 	/**
-	 * Affiche les données brutes dans le corps de la page, dans le VBox
+	 * Affiche dans la vue les données d'une liste dans un layout définie. 
+	 *
+	 * @param layout : le layout où il faut afficher les données
+	 * @param donnees : les données à afficher
 	 */
-	private void affichageDonnees() {
-		if (stockage == null) {
-			vboxDonnees.getChildren().add(new Label("Aucune données dans l'app !"));
-			
-		} else {
-			ArrayList<Employe> employes = stockage.getListeEmploye();
-			ArrayList<Salle> salles = stockage.getListeSalle();
-			ArrayList<Activite> activitees = stockage.getListeActivite();
-			ArrayList<Reservation> reservations = stockage.getListeReservation();
-			
-			Label employe = new Label("Employés :");
-			vboxDonnees.getChildren().add(employe);
-			
-			for (Employe e : employes) {
-				String infos = e.toString();
-				Label affichageInfos = new Label(infos);
-				vboxDonnees.getChildren().add(affichageInfos);
-			}
-			
-			
-			Label salle = new Label("Salles :");
-			vboxDonnees.getChildren().add(salle);
-			
-			for (Salle s : salles) {
-				String infos = s.toString();
-				Label affichageInfos = new Label(infos);
-				vboxDonnees.getChildren().add(affichageInfos);
-			}
-			
-			
-			Label activite = new Label("Activitées :");
-			vboxDonnees.getChildren().add(activite);
-			
-			for (Activite a : activitees) {
-				String infos = a.toString();
-				Label affichageInfos = new Label(infos);
-				vboxDonnees.getChildren().add(affichageInfos);
-			}
-			
-			
-			Label reservation = new Label("Réservations :");
-			vboxDonnees.getChildren().add(reservation);
-			
-			for (Reservation r : reservations) {
-				String infos = r.toString();
-				Label affichageInfos = new Label(infos);
-				vboxDonnees.getChildren().add(affichageInfos);
-			}
-			
+	private void affichage(Pane layout, ArrayList<? extends Object> donnees) {
+		for (Object donnee : donnees) {
+			String infos = donnee.toString();
+			Label affichageInfos = new Label(infos);
+			layout.getChildren().add(affichageInfos);
 		}
-		
 	}
-	
 }
---
+++ workspace/RoomManager/src/modeles/consultation/Consultation.java
@@ -0,0 +1,74 @@
+/*
+ * 
+ */
+
+package modeles.consultation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import lanceur.RoomManager;
+import modeles.items.Activite;
+import modeles.items.Employe;
+import modeles.items.Reservation;
+import modeles.items.Salle;
+import modeles.stockage.Stockage;
+
+/**
+ * 
+ */
+public class Consultation {
+	
+	/** Données de l'application à exploiter */
+	private Stockage stockage;
+	
+	/**
+	 * Initialise le Stockage
+	 */
+	private void initialisationStockage() {
+		stockage = RoomManager.stockage;
+	}
+	
+	/**
+	 * Retourne toutes les données brutes de l'application
+	 * avec un ajout d'une valeur par defaut si des parties de stockages sont null.
+	 * 
+	 * @return les données brutes
+	 */
+	public HashMap<String, ArrayList<? extends Object>> fetchDonneesBrutes() {
+		HashMap<String, ArrayList<? extends Object>> donnees = new HashMap<>();
+		
+		initialisationStockage();
+		if (stockage == null) {
+			donnees.put("pas de données", new ArrayList<String>(Arrays.asList("Aucune données enregistré")));
+		
+		} else {
+			ArrayList<Employe> employes = stockage.getListeEmploye();
+			ArrayList<Salle> salles = stockage.getListeSalle();
+			ArrayList<Activite> activitees = stockage.getListeActivite();
+			ArrayList<Reservation> reservations = stockage.getListeReservation();
+			
+			donnees.put("Employés", employes);
+			donnees.put("Salles", salles);
+			donnees.put("Activitées", activitees);
+			donnees.put("Réservations", reservations);
+			
+			initialiseValeurDefautBrute(donnees);
+		}
+		
+		return donnees;
+	}
+	
+	/**
+	 * Initialise par avec des valeurs par defauts si des parties de stockages sont null
+	 * @param donnees : les données de l'app organisé.
+	 */
+	private void initialiseValeurDefautBrute(HashMap<String, ArrayList<? extends Object>> donnees) {
+		for (String key : donnees.keySet()) {
+			if (donnees.get(key) == null) {
+				donnees.put(key, new ArrayList<>(Arrays.asList("Aucun(e) " + key + " enregistré dans l'application")));
+			}
+		}
+	}
+}
