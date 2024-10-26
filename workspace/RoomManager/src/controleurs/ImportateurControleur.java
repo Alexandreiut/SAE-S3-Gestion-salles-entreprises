7a39f50ec83cb5693f081d19e4e3ef34b3bd7236
Astier Adrien <adrien.astier@gmail.com>
2024-10-26 18:15:03 +0200

    Ajout IHM des vues Importation et Exportation

--- workspace/RoomManager/src/controleurs/ImportateurControleur.java
+++ workspace/RoomManager/src/controleurs/ImportateurControleur.java
@@ -5,11 +5,19 @@
 
 package controleurs;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import affichages.GestionAffichageMenu;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import modeles.NavigationVues;
+import modeles.consultation.Consultation;
 
 /**
  * Controleur de la vue importateur.fxml
@@ -19,8 +27,14 @@ public class ImportateurControleur {
 	@FXML
 	private Pane panePrincipal;
 	
+	@FXML
+	private VBox vboxLocale;
+	
 	@FXML 
 	private Button boutonRetour;
+	
+	@FXML
+	private Text textSelection;
 	
 	@FXML
 	private void menu() {
