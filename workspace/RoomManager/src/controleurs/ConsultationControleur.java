c0b6d9ead9d6defd51b59e336ee295fa94512372
ADRIEN ASTIER <adrien.astier@iut.local>
2024-11-04 10:29:14 +0100

    Sauvegarde locale

--- workspace/RoomManager/src/controleurs/ConsultationControleur.java
+++ workspace/RoomManager/src/controleurs/ConsultationControleur.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import modeles.NavigationVues;
 import modeles.consultation.Consultation;
 
