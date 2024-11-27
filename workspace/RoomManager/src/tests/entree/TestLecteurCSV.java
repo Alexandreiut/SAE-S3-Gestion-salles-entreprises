f2ae762e0e6a8172183a04229d4e488786cfada7
ADRIEN ASTIER <adrien.astier@iut.local>
2024-11-27 08:11:03 +0100

    Sauvegarde locale

--- workspace/RoomManager/src/tests/entree/TestLecteurCSV.java
+++ workspace/RoomManager/src/tests/entree/TestLecteurCSV.java
@@ -72,19 +72,19 @@ class TestLecteurCSV {
 		attenduValide.add(new Salle("00000001", "A6", 15, true, false, 4, "PC portable", logiciel1, false));
 		
 		lignesSallesInvalide.add("entete");
-		lignesSallesInvalide.add("0000000H;@;2e0;oui;non;e;PC4 Windows;bureautique,java,Intellij;non");
+		lignesSallesInvalide.add("0000000H;#;2e0;oui;non;e;PC4 Windows;bureautique,java,Intellij;non");
 		
 		logiciel2.add("bureautique");
 		logiciel2.add("java");
 		logiciel2.add("Intellij");
-		attenduInvalide.add(new Salle("Id inconnu", "Nom inconnu", 0, true, false, 0, "Type inconnu", logiciel2, false));
+		attenduInvalide.add(new Salle("Id inconnu", "#", 0, true, false, 0, "Type inconnu", logiciel2, false));
 		
 		ArrayList<Object> resultatValide = LecteurCSV.lireSalleCSV(lignesSallesValide);
 		
 		ArrayList<Object> resultatInvalide = LecteurCSV.lireSalleCSV(lignesSallesInvalide);
 		
-		assertTrue(attenduValide.toString().equals(resultatValide.toString()));
-		assertTrue(attenduInvalide.toString().equals(resultatInvalide.toString()));
+		assertEquals(attenduValide.toString(),resultatValide.toString());
+		assertEquals(attenduInvalide.toString(), resultatInvalide.toString());
 	}
 
 	@Test
